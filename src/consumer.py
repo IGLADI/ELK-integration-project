6a6d8e3ccc537afcba88698a4a5800c2ae1aaa94
IGLADI <jarno.roels@gmail.com>
2024-04-03 22:25:58 +0200

    add rabbitmq queue & routing key to .env

--- src/consumer.py
+++ src/consumer.py
@@ -12,18 +12,22 @@ def main():
     password = os.getenv("RABBITMQ_PASSWORD")
     host = os.getenv("RABBITMQ_HOST")
     virtual_host = os.getenv("RABBITMQ_VIRTUAL_HOST")
+    queue = os.getenv("RABBITMQ_QUEUE")
+    routing_key = os.getenv("RABBITMQ_ROUTING_KEY")
     print("Connecting to RabbitMQ with the following credentials:")
     print(f"Username: {username}")
     print(f"Password: {password}")
     print(f"Host: {host}")
     print(f"Virtual Host: {virtual_host}")
+    print(f"Queue: {queue}")
+    print(f"Routing Key: {routing_key}")
 
     credentials = pika.PlainCredentials(username, password)
     connection = pika.BlockingConnection(pika.ConnectionParameters(host=host, virtual_host=virtual_host, credentials=credentials))
     channel = connection.channel()
 
     # not sure if it will have CAPS or not
-    channel.queue_declare(queue="Heartbeat")
+    channel.queue_declare(queue=queue)
 
     # pylance lies, callbacks call 4 args
     def callback(ch, method, properties, body):
@@ -40,7 +44,7 @@ def main():
             print("\33[31mError parsing XML\33[0m")
 
     # this queue = routing key from publisher
-    channel.basic_consume(queue="Heartbeat", on_message_callback=callback, auto_ack=True)
+    channel.basic_consume(queue=routing_key, on_message_callback=callback, auto_ack=True)
     print("Waiting for msgs.")
     channel.start_consuming()
 
