25f42004512cc2d5cdfeed1e856d6d40d9dff606
IGLADI <jarno.roels@gmail.com>
2024-04-23 11:00:57 +0200

    parse args instead of responding questions (launch script)

--- src/main.bash
+++ src/main.bash
@@ -1,7 +1,6 @@
 # run this script as root
 if [ $(/usr/bin/id -u) -ne 0 ]; then
-    echo "This script must be run as root"
-    exit
+    echo "We recommend running this script as root"
 fi
 
 # go into the root directory of the project
@@ -15,34 +14,78 @@ chmod -R 777 ./ELK/elasticsearch/data/
 
 cd src
 
+start=false
+
+# take all args and set them instead of the questions:
+ELASTIC_VERSION=$2
+ELASTIC_PASSWORD=$3
+KIBANA_SYSTEM_PASSWORD=$4
+RABBITMQ_USERNAME=$5
+RABBITMQ_PASSWORD=$6
+RABBITMQ_HOST=$7
+RABBITMQ_VIRTUAL_HOST=$8
+RABBITMQ_QUEUE=$9
+
 # not case senitive
 if [[ "${1,,}" == "setup" ]]; then
     # just to tell  the user in what mode the script is running
     echo "Setting up for the first time"
 
-    echo "What version of elasticsearch do you want to use? (current latest is 8.12.2)"
-    read ELASTIC_VERSION
+    if [ -z "$ELASTIC_VERSION" ]; then
+        echo "What version of elasticsearch do you want to use? (current latest is 8.12.2)"
+        read ELASTIC_VERSION
+    else
+        echo "found ELASTIC_VERSION=$ELASTIC_VERSION"
+    fi
 
-    echo "What password do you want to use for elasticsearch?"
-    read -s ELASTIC_PASSWORD
+    if [ -z "$ELASTIC_PASSWORD" ]; then
+        echo "What password do you want to use for elasticsearch?"
+        read -s ELASTIC_PASSWORD
+    else
+        echo "found ELASTIC_PASSWORD=$ELASTIC_PASSWORD"
+    fi
 
-    echo "What password do you want to use for kibana?"
-    read -s KIBANA_SYSTEM_PASSWORD
+    if [ -z "$KIBANA_SYSTEM_PASSWORD" ]; then
+        echo "What password do you want to use for kibana?"
+        read -s KIBANA_SYSTEM_PASSWORD
+    else
+        echo "found KIBANA_SYSTEM_PASSWORD=$KIBANA_SYSTEM_PASSWORD"
+    fi
 
-    echo "What is your rabbitmq username?"
-    read RABBITMQ_USERNAME
+    if [ -z "$RABBITMQ_USERNAME" ]; then
+        echo "What is your rabbitmq username?"
+        read RABBITMQ_USERNAME
+    else
+        echo "found RABBITMQ_USERNAME=$RABBITMQ_USERNAME"
+    fi
 
-    echo "What is your rabbitmq password?"
-    read -s RABBITMQ_PASSWORD
+    if [ -z "$RABBITMQ_PASSWORD" ]; then
+        echo "What is your rabbitmq password?"
+        read -s RABBITMQ_PASSWORD
+    else
+        echo "found RABBITMQ_PASSWORD=$RABBITMQ_PASSWORD"
+    fi
 
-    echo "What is your rabbitmq host?"
-    read RABBITMQ_HOST
+    if [ -z "$RABBITMQ_HOST" ]; then
+        echo "What is your rabbitmq host?"
+        read RABBITMQ_HOST
+    else
+        echo "found RABBITMQ_HOST=$RABBITMQ_HOST"
+    fi
 
-    echo "What is your rabbitmq virtual host?"
-    read RABBITMQ_VIRTUAL_HOST
+    if [ -z "$RABBITMQ_VIRTUAL_HOST" ]; then
+        echo "What is your rabbitmq virtual host?"
+        read RABBITMQ_VIRTUAL_HOST
+    else
+        echo "found RABBITMQ_VIRTUAL_HOST=$RABBITMQ_VIRTUAL_HOST"
+    fi
 
-    echo "What is your rabbitmq queue where the heartbeats will be published?"
-    read RABBITMQ_QUEUE
+    if [ -z "$RABBITMQ_QUEUE" ]; then
+        echo "What is your rabbitmq queue where the heartbeats will be published?"
+        read RABBITMQ_QUEUE
+    else
+        echo "found RABBITMQ_QUEUE=$RABBITMQ_QUEUE"
+    fi
 
     # somtimes buggy when we don't start it before
     docker compose up -d
@@ -92,4 +135,6 @@ if [[ "${1,,}" == "--help" ]]; then
     echo "  leave blank: start the environment"
     echo "  stop: stop the environment"
     echo "  --help: show this message"
+    echo "  instead of relying on the questions you can pass the following arguments:"
+    echo "  ./main.bash setup <ELASTIC_VERSION> <ELASTIC_PASSWORD> <KIBANA_SYSTEM_PASSWORD> <RABBITMQ_USERNAME> <RABBITMQ_PASSWORD> <RABBITMQ_HOST> <RABBITMQ_VIRTUAL_HOST> <RABBITMQ_QUEUE>"
 fi
