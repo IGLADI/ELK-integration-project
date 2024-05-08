85f7a357f6b5b313129a1afe71d703478e9d11d5
IGLADI <jarno.roels@gmail.com>
2024-05-08 08:48:19 +0000

    add finish message on setup, remove setup container on first down remove docker compose version (deprecated), remove http orphants on dashboard

--- src/main.bash
+++ src/main.bash
@@ -5,17 +5,20 @@ if [ $(/usr/bin/id -u) -ne 0 ]; then
     echo "We recommend running this script as root"
 fi
 
+cd src || echo "already in src"
+
 fix_permissions() {
     # chmod just in case
-    sudo chmod -R go-w ../ELK/heartbeat/services/
     # should be changed with untrusted users
     sudo chmod -R 777 ../ELK/elasticsearch/data/
-    sudo chmod +rwx ../src/setup/entrypoint.sh
-    sudo chmod go-w ../ELK/heartbeat/heartbeat.yml
-    # change the owner to root
-    sudo chown 1000:1000 ../ELK/heartbeat/heartbeat.yml
+    sudo chmod +rwx ./setup/entrypoint.sh
 }
 fix_permissions
+finish() {
+    echo "Now you can access kibana at http://localhost:16601/app/dashboards#/view/f3e771c0-eb19-11e6-be20-559646f8b9ba?_g=(filters:!(),refreshInterval:(pause:!f,value:1000),time:(from:now-24h%2Fh,to:now))"
+    echo "Note that you should replace localhost with the ip of the machine where the docker containers are running if you are accessing from another machine"
+    echo "Go check out the readme for future steps and setup your first services to monitor"
+}
 
 # take all args and set them instead of the questions:
 ELASTIC_VERSION=$2
@@ -102,31 +105,36 @@ if [[ "${1,,}" == "setup" ]]; then
 
     # force recreate just in case the network is bugged (due to a previous version)
     docker compose up setup --force-recreate -d
-    sleep 120s
+    # better if we way, more clean but can actually be skipped (will be slower as the rest will constantly restart withouth this sleep)
+    sleep 60s
     fix_permissions
     docker compose up setup-export --force-recreate -d
 
     # 1 restart for sealf healing
     docker compose up -d && docker compose down && docker compose up -d
 
-    # we could remove setup containers & images but they use 0 resources
+    # we could remove setup containers & images but they use 0 resources, will be down on first down
+    finish
 fi
 
 # if no args
 if [ -z "$1" ]; then
     echo "Starting as normal"
-    start=true
     docker compose up -d
 
-    echo "Now you can access kibana at http://localhost:16601/app/dashboards#/view/f3e771c0-eb19-11e6-be20-559646f8b9ba?_g=(filters:!(),refreshInterval:(pause:!f,value:1000),time:(from:now-24h%2Fh,to:now))"
-    echo "Note that you should replace localhost with the ip of the machine where the docker containers are running if you are accessing from another machine"
-    echo "Go check out the readme for future steps and setup your first services to monitor"
+    finish
 fi
 
 if [[ "${1,,}" == "stop" || "${1,,}" == "down" ]]; then
     docker compose down
     # clear the src-consumer container image so it will be updated if changed on next up
     docker image rm src-consumer:latest --force
+
+    # remove setups if active
+    docker compose down setup setup-export | echo "no setup containers to stop:"
+    docker rmi src-setup src-setup-export | echo "no setup images to remove:"
+
+    echo "Stopped the environment"
 fi
 
 if [[ "${1,,}" == "--help" ]]; then
